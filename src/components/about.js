d8fc465428d35481115b7c966df3ce41ceb59a12
jackskim <jackskim29@gmail.com>
2020-11-17 10:00:47 -0800

    feat: Add About section

---
+++ src/components/about.js
@@ -0,0 +1,61 @@
+import React from "react"
+import styled from "styled-components"
+import { breakpoints } from "../styles/breakpoints"
+
+const AboutContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.6rem;
+  color: #696969;
+  line-height: 1.6;
+
+  @media (min-width: ${breakpoints.tabletMin}) {
+    margin-top: 5%;
+  }
+`
+
+const H2 = styled.h2`
+  font-size: 2.8rem;
+  font-weight: 600;
+  color: #222;
+  margin-bottom: 3%;
+`
+
+const Section = styled.section`
+  @media (min-width: ${breakpoints.tabletMin}) {
+    max-width: 70%;
+  }
+`
+
+const P = styled.p`
+  margin: 1rem auto;
+`
+
+const About = () => (
+  <AboutContainer>
+    <H2>ABOUT</H2>
+    <Section>
+      <P>
+        In April 2019, I committed to becoming a software engineer by enrolling
+        in Lambda School and graduated from their part-time (18-month) Full
+        Stack Web & Computer Science track in August 2020.
+      </P>
+
+      <P>
+        I love perfecting the details of my craft and apply creativity and
+        rationality to everything I do. I believe the most challenging goals can
+        be accomplished with enough insight, understanding, and desire. I aim
+        for clarity of understanding to achieve consistent, high performance.
+      </P>
+
+      <P>
+        My mission is to provide users with software that simplifies and removes
+        pain points from their lives while respecting their privacy. I'm aiming
+        to contribute scalable, robust, and maintainable code to a team that
+        aligns with this mission.
+      </P>
+    </Section>
+  </AboutContainer>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import About from "../components/about"
 
 const Home = () => (
   <Layout>
     <Hero />
+    <About />
   </Layout>
 )
 
