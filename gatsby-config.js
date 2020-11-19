9ad87eb804ed0665da54748c02e61f3f32658517
jackskim <jackskim29@gmail.com>
2020-11-18 16:31:06 -0800

    feat: add example content and MDX + image rendering support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,36 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+        gatsbyRemarkPlugins: [{ resolve: "gatsby-remark-images" }],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `fonts`,
         path: `${__dirname}/static/fonts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts/`,
       },
     },
     {
---
+++ src/templates/post.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../components/layout"
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`
+
+const PostTemplate = ({ data: { mdx: post } }) => {
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      <p>Posted by {post.frontmatter.author}</p>
+      <MDXRenderer>{post.body}</MDXRenderer>
+      <Link to="/">&larr; back to all posts</Link>
+    </Layout>
+  )
+}
+
+export default PostTemplate
