8066ebd5ceeb0a70f3d8aab34702863327fbc5da
jackskim <jackskim29@gmail.com>
2020-11-20 15:27:40 -0800

    chore: adjust project preview spacing

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -15,6 +15,10 @@ const Article = styled.article`
 
   p {
     margin: 5% 0;
+
+    @media (min-width: ${breakpoints.sm}) {
+      margin: 1% 0;
+    }
   }
 `
 
@@ -25,13 +29,15 @@ const ProjectImage = styled(Image)`
 
 const H3 = styled.h3`
   font-size: 2.2rem;
-  margin: 5% 0;
+  margin: 15px 0 5px;
 `
 const ProjectLink = styled(Link)`
+  display: inline-block;
   font-weight: 600;
-  padding-bottom: 3px;
+  padding-bottom: 0;
   border-bottom: 2px solid #222;
   transition: 0.3s;
+  margin-top: 10px;
 
   :hover {
     border-bottom: 2px solid transparent;
