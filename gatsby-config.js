d02dd4f2710daaafa7fa16184a662cf0a4b46048
jackskim <jackskim29@gmail.com>
2020-11-23 12:08:48 -0800

    feat: add webpack bundle analyzer and production build

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,5 +50,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-webpack-bundle-analyzer",
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: "static",
+      },
+    },
   ],
 }
